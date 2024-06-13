d6ad1fc91adedb61cb9b1049e7ed10fd27a3cedb
nininjaz <lannycarolinyalves@gmail.com>
2024-06-13 09:55:55 -0300

    rs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+
 const App = () => {
   const [color] = useState("#a54fcc")
   document.body.style.backgroundColor = color
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import logo from '../assets/react.svg'
-
+import {Link} from 'react-router-dom'
 const Home = () => {
 
     return (
